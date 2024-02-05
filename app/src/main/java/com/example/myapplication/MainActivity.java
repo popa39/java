93a4dbcf6a545db3b08d576b9a044933f3c44f94
popa39 <152579041+popa39@users.noreply.github.com>
2024-02-05 21:13:57 +0900

    ура блядь наконец то эта залупа закончена

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -39,24 +39,3 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 }
-class NewActivity extends AppCompatActivity {
-
-    @SuppressLint("SetTextI18n")
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        FrameLayout frameLayout = new FrameLayout(this);
-        TextView textView = new TextView(this);
-        textView.setText("Hello World!");
-
-        FrameLayout.LayoutParams layoutParams = new FrameLayout.LayoutParams
-                (FrameLayout.LayoutParams.WRAP_CONTENT, FrameLayout.LayoutParams.WRAP_CONTENT);
-        layoutParams.gravity = Gravity.CENTER_HORIZONTAL | Gravity.TOP;
-
-        textView.setLayoutParams(layoutParams);
-        textView.setTextSize(26);
-        frameLayout.addView(textView);
-        setContentView(frameLayout);
-    }
-}
